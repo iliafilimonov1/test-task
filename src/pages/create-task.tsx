d4b7abcb4a8054d97b9edcb5353796cb785d36b2
Ilia Filimonov <9991571155i@gmail.com>
2022-08-16 13:06:08 +0300

    posibility to create new task

--- src/pages/create-task.tsx
+++ src/pages/create-task.tsx
@@ -3,16 +3,30 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { useMutation } from "react-query";
 
-import { TaskService, ITask } from "../app/services/task.service";
+import { TaskService } from "../app/services/task.service";
+import { ITask, Reminder } from "../app/Types";
 import styles from "../styles/Home.module.css";
 
 const CreateTask: NextPage = () => {
   const [data, setData] = useState<ITask>({
-    id: 8,
+    id: Math.random() * 1000,
     date: "",
+    reminder: [],
   } as ITask);
 
+  const [reminder, setReminder] = useState<Reminder>();
+
   const { push } = useRouter();
+
+  const addNewReminderHandler = () => {
+    if (reminder?.title && reminder?.description) {
+      setData({
+        ...data,
+        reminder: [...data.reminder, reminder],
+      });
+    }
+    setReminder(undefined);
+  };
 
   const { isLoading, mutateAsync } = useMutation(
     "create task",
@@ -39,19 +53,7 @@ const CreateTask: NextPage = () => {
       <form onSubmit={handleSubmit}>
         <div className={styles.task_wrapper}>
           <input
-            label="Task id"
-            placeholder="Please enter id"
-            value={data.id}
-            onChange={(e) =>
-              setData({
-                ...data,
-                id: +e.target.value,
-              })
-            }
-          />
-
-          <input
-            type="text"
+            type="date"
             placeholder="Please enter date"
             value={data.date}
             onChange={(e) =>
@@ -61,6 +63,48 @@ const CreateTask: NextPage = () => {
               })
             }
           />
+          {data.reminder &&
+            data.reminder.map((reminder) => (
+              <div key={reminder.title}>
+                <h4>{reminder.title}</h4>
+                <h4>{reminder.description}</h4>
+              </div>
+            ))}
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              setReminder({ title: "", description: "", isEnded: false });
+            }}
+          >
+            Add new reminder
+          </button>
+          {reminder && (
+            <div>
+              <input
+                type="text"
+                placeholder="Please enter title"
+                value={reminder.title}
+                onChange={(e) =>
+                  setReminder({
+                    ...reminder,
+                    title: e.target.value,
+                  })
+                }
+              />
+              <input
+                type="text"
+                placeholder="Please enter description"
+                value={reminder.description}
+                onChange={(e) =>
+                  setReminder({
+                    ...reminder,
+                    description: e.target.value,
+                  })
+                }
+              />
+              <button onClick={addNewReminderHandler}>Ok</button>
+            </div>
+          )}
 
           <button disabled={isLoading}>Create task</button>
         </div>
